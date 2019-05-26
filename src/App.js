055b02f5e8f81300b3a95ca191df0783d1439e56
sanra9 <sanrra999@gmail.com>
2019-05-26 14:54:29 -0500

    Fix add button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(){
+    super()
+    this.state = {
+      count: 0,
+    }
+  }
+
+incrementNum = () => {
+  console.log(this.state)
+  this.setState((state) => ({ count: state.count + 1}))
+}
+
+decrementNum = () => {
+  this.setState((state) => ({ count: state.count - 1}))
+}
+
   render() {
+    const { count } = this.state
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{count}</span>
+        <button id="inc" onClick={this.incrementNum}>Incrementa</button>
+        <button id="inc" onClick={this.decrementNum}>Decrementa</button>
       </div>
     );
   }
 }
+
+
 
 export default App;
